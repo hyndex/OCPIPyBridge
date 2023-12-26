258161fe0391ca8e19e33485112de45ec0196f82
diki bhuyan <dikibhuyan@gmail.com>
2023-12-27 03:24:09 +0530

    new imports added

--- OCPIPyBridge/_init__.py
+++ OCPIPyBridge/_init__.py
@@ -1,9 +1,13 @@
 # __init__.py for py_ocpi package
 
 from .models import (
-    Dimension, Token, Connector, Logo, BusinessDetails,
-    PriceComponent, TariffElement, Tariff, Meter, Reservation,
-    Feedback, Coordinates, OpeningTimes, RegularHours, ExceptionalPeriod,
-    Facility, EnergyMix, Image, Location, EVSE, ChargingPeriod, CDR,
-    Command, Transaction, User
+    Dimension, Token, Connector, Logo, BusinessDetails, Credentials,
+    PriceComponent, TariffElement, Tariff, Meter, Reservation, Feedback,
+    Coordinates, OpeningTimes, RegularHours, ExceptionalPeriod, Facility,
+    EnergyMix, Image, Location, EVSE, ChargingPeriod, CDR, Command,
+    Transaction, User, ChargingProfilePeriod, ChargingProfile,
+    ActiveChargingProfile, ChargingProfileResponse, ActiveChargingProfileResult,
+    ChargingProfileResult, ClearProfileResult, SetChargingProfile,
+    CommandResult, CommandResponse, DisplayText, EnergyContract,
+    LocationReferences, AuthorizationInfo
 )
